a834ecea4a319db7d41fdfb9e8f19fae4b06a871
olegator072@gmail.com <Asser220_>
2019-01-08 01:51:33 +0300

    Test Chat

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var WebSocket = require("ws");
+const server = new WebSocket.Server({port: 3001})
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+server.on('connection', ws => {
+  ws.on('message', message => {
+    server.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    })
+  })
+  //ws.send('Соединение с сервером установлено!');
+});
+ 
+app.listen(3000, () => {
+  console.log('Сервер запущен!');
+})
